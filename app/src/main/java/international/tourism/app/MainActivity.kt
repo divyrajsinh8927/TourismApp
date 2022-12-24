5b932eeab108a372e762485fb634712ed67b119d
divyrajsinh8927 <divyrajsinh8927@gmail.com>
2022-12-24 12:48:15 +0530

    last

--- app/src/main/java/international/tourism/app/MainActivity.kt
+++ app/src/main/java/international/tourism/app/MainActivity.kt
@@ -3,74 +3,82 @@ package international.tourism.app
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
-import com.andremion.floatingnavigationview.FloatingNavigationView
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import international.tourism.app.models.Booking
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mFloatingNavigationView: FloatingNavigationView
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private lateinit var placeFragment: PlaceFragment
+    private lateinit var hotelFragment: HotelFragment
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var bookingFragment: BookingFragment
+
     private lateinit var sharedPref: SharedPreferences
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sharedPref = getSharedPreferences("tourism_pref", MODE_PRIVATE)
-        val aEmail = sharedPref.getString("email",null)
-        if(aEmail == null)
-            sendToHome()
 
-        /*  Start Navigation view */
+        bottomNavigationView = findViewById(R.id.bottomNavBar)
+        bottomNavigationView.setOnItemSelectedListener(::bottomItemNavSelected)
 
-        mFloatingNavigationView =
-            findViewById<View>(R.id.floating_navigation_view) as FloatingNavigationView
-        mFloatingNavigationView!!.setOnClickListener { mFloatingNavigationView!!.open() }
-        mFloatingNavigationView!!.setNavigationItemSelectedListener {item ->
+        homeFragment = HomeFragment()
+        placeFragment = PlaceFragment()
+        hotelFragment = HotelFragment()
+        bookingFragment = BookingFragment()
 
-             when (item.itemId) {
-                R.id.about -> {
-                    startActivity(Intent(this, AboutusActivity::class.java))
-                    true
-                }
-                R.id.share ->{
-                    val intent= Intent()
-                    intent.action=Intent.ACTION_SEND
-                    intent.putExtra(Intent.EXTRA_TEXT,"Hey Check out this Great app:")
-                    intent.type="text/plain"
-                    startActivity(Intent.createChooser(intent,"Share To:"))
-                    true
-                }
-                 R.id.btnLogout ->{
-                     sharedPref.edit().remove("email").apply()
-                     sharedPref.edit().remove("id").apply()
-                     startActivity(Intent(this,LoginActivity::class.java))
-                 }
-                 R.id.profile -> {
-                     startActivity(Intent(this, ProfileActivity::class.java))
-                     true
-                 }
-                else -> super.onOptionsItemSelected(item)
-            }
-
-            mFloatingNavigationView!!.close()
-            true
-        }
+        showHomeFragment()
     }
-
-    override fun onBackPressed() {
-        if (mFloatingNavigationView!!.isOpened) {
-            mFloatingNavigationView!!.close()
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    /* End Navigation view */
-    private fun sendToHome()
+    private fun bottomItemNavSelected(item: MenuItem): Boolean
     {
-        val intent = Intent(this,LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+        when(item.itemId)
+        {
+            R.id.btnHome -> showHomeFragment()
+            R.id.btnPlace -> showPlaceFragment()
+            R.id.btnHotel -> showHotelFragment()
+            R.id.btnBookings -> showBookingFragment()
+            else -> return false
+        }
+
+        return true
     }
+
+    private fun showHomeFragment()
+    {
+        val manager = supportFragmentManager.beginTransaction()
+        manager.replace(R.id.fragmentContainer,homeFragment)
+        manager.commit()
+    }
+
+    private fun showPlaceFragment()
+    {
+        val manager = supportFragmentManager.beginTransaction()
+        manager.replace(R.id.fragmentContainer,placeFragment)
+        manager.commit()
+    }
+
+    private fun showHotelFragment()
+    {
+        val manager = supportFragmentManager.beginTransaction()
+        manager.replace(R.id.fragmentContainer,hotelFragment)
+        manager.commit()
+    }
+
+    private fun showBookingFragment()
+    {
+        val manager = supportFragmentManager.beginTransaction()
+        manager.replace(R.id.fragmentContainer,homeFragment)
+        manager.commit()
+    }
+
+
+
+
 }
---
+++ app/src/main/java/international/tourism/app/PlaceFragment.kt
@@ -0,0 +1,14 @@
+package international.tourism.app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class PlaceFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+}
