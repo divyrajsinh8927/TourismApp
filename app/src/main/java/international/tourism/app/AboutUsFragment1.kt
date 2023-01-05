f3813943ec68fcc0505e3ab30c0a9aa62259a5b7
divyrajsinh8927 <divyrajsinh8927@gmail.com>
2023-01-05 09:24:28 +0530

    aboutUsDesign

---
+++ app/src/main/java/international/tourism/app/AboutUsFragment1.kt
@@ -0,0 +1,19 @@
+package international.tourism.app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AboutUsFragment1 : Fragment()
+{
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_us_1, container, false)
+    }
+}
