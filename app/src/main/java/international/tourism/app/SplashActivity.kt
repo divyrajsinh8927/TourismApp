1fad8b4fbe698e98a547aabb853afc67c59d6a6a
divyrajsinh8927 <divyrajsinh8927@gmail.com>
2023-02-09 11:03:28 +0530

    Implement Splash Screen

---
+++ app/src/main/java/international/tourism/app/SplashActivity.kt
@@ -0,0 +1,28 @@
+package international.tourism.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doSomething()
+        }, 2000)
+    }
+
+    private fun doSomething()
+    {
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+
+
+}
