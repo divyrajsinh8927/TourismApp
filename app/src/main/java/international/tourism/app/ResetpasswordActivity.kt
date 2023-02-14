4054853eda8ba6344ca6cea8f315534cc2034f3e
Divyrajsinh Vala <divyrajsinh8927@gmail.com>
2023-02-14 11:22:46 +0530

    User login,register,forgetPassword,resetPassword Completeed Design

---
+++ app/src/main/java/international/tourism/app/ResetpasswordActivity.kt
@@ -0,0 +1,13 @@
+package international.tourism.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetpasswordActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resetpassword)
+    }
+}
